3ceb0d9165a333a1e26f745ef28ab07b4c7afa2f
faiyazbits <m.faiyaz123@gmail.com>
2019-09-06 11:18:55 +0530

    generated individual components

--- src/app/feature/trending/trending.module.ts
+++ src/app/feature/trending/trending.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TrendingRoutingModule } from './trending-routing.module';
+import { TrendingListComponent } from './components/trending-list/trending-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TrendingListComponent],
   imports: [
     CommonModule,
     TrendingRoutingModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DrawerComponent } from './drawer/drawer.component';
 import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 
 
 @NgModule({
-  declarations: [DrawerComponent, YoutubePlayerComponent],
+  declarations: [DrawerComponent, YoutubePlayerComponent, UserSearchComponent],
   imports: [
     CommonModule
   ]
