8e586ccc25baa20da095561da1042cdd2e0e9238
faiyazbits <m.faiyaz123@gmail.com>
2019-09-10 11:48:38 +0530

    ported login component

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.loginForm = fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(15)
+        ])
+      ]
+    });
   }
 
+  public hasError(controlName: string, errorName: string) {
+    return this.loginForm.controls[controlName].hasError(errorName);
+  }
+
+  onSubmit(values) {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authService.loginWithCredentials(values);
+  }
 }
