a7be013b4cffe22ccd1a0cf1dac87d8295982694
faiyazbits <m.faiyaz123@gmail.com>
2019-09-11 16:52:39 +0530

    admin task view search email implemented

--- src/app/feature/admin-task/pages/admin-task-view/admin-task-view.component.ts
+++ src/app/feature/admin-task/pages/admin-task-view/admin-task-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { AdminTaskService } from '../../services/admin-task.service';
+import { MatTableDataSource } from '@angular/material';
 
 interface AdminTaskDatum {
   email: string;
@@ -14,11 +15,19 @@ interface AdminTaskDatum {
 export class AdminTaskViewComponent implements OnInit {
   allUserAdminTaskData$: Observable<AdminTaskDatum[]> = of([]);
   displayedColumns: string[] = ['email', 'tasks', 'actions'];
+  dataSource;
   constructor(private adminTaskService: AdminTaskService) {
     this.allUserAdminTaskData$ = this.adminTaskService.allUserAdminTaskDataForTable$;
+    this.allUserAdminTaskData$.subscribe(allUserAdminTasks => {
+      this.dataSource = new MatTableDataSource(allUserAdminTasks);
+    });
   }
 
   ngOnInit() {
     this.adminTaskService.findAllAdminTasks();
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
