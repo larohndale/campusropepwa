49a296669e44cd70dcaa2d1e0d94437deb47ee33
faiyazbits <m.faiyaz123@gmail.com>
2019-10-02 13:29:32 +0530

    helpline add input material error fixed

---
+++ src/app/feature/helpline/components/helpline-form/helpline-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-helpline-form',
+  templateUrl: './helpline-form.component.html',
+  styleUrls: ['./helpline-form.component.scss']
+})
+export class HelplineFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/feature/helpline/components/helpline.components.module.ts
+++ src/app/feature/helpline/components/helpline.components.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HelplineListComponent } from './helpline-list/helpline-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HelplineAddEditComponent } from './helpline-add-edit/helpline-add-edit.component';
+import { HelplineFormComponent } from './helpline-form/helpline-form.component';
 
 @NgModule({
-  declarations: [HelplineListComponent, HelplineAddEditComponent],
+  declarations: [HelplineListComponent, HelplineAddEditComponent, HelplineFormComponent],
   imports: [CommonModule, SharedModule],
   exports: [HelplineListComponent, HelplineAddEditComponent]
 })
