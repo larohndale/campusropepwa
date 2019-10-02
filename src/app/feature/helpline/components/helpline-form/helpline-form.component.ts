a6185589b5c4e0b76c0941576cfd5b64e1f8de81
umarfarooq95 <mohamedumar931@gmail.com>
2019-10-02 18:29:01 +0530

    code ported from add-edit component to form component

--- src/app/feature/helpline/components/helpline-form/helpline-form.component.ts
+++ src/app/feature/helpline/components/helpline-form/helpline-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IState } from 'src/app/core/models/state';
+import { ConstantsService } from 'src/app/core/core.module';
 
 @Component({
   selector: 'app-helpline-form',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelplineFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() formGroup: FormGroup;
+  @Input() titleAlert: string;
+
+  states: IState[];
+  selectedState = '';
+
+  constructor(private constantsService: ConstantsService) { }
 
   ngOnInit() {
+    this.states = this.constantsService.getStates();
+  }
+
+  onStateChange(selectedState: string) {
+    this.formGroup.value.state = selectedState;
   }
 
 }
--- src/app/feature/helpline/components/helpline.components.module.ts
+++ src/app/feature/helpline/components/helpline.components.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelplineListComponent } from './helpline-list/helpline-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { HelplineAddEditComponent } from './helpline-add-edit/helpline-add-edit.component';
 import { HelplineFormComponent } from './helpline-form/helpline-form.component';
 
 @NgModule({
-  declarations: [HelplineListComponent, HelplineAddEditComponent, HelplineFormComponent],
+  declarations: [HelplineListComponent, HelplineFormComponent],
   imports: [CommonModule, SharedModule],
-  exports: [HelplineListComponent, HelplineAddEditComponent]
+  exports: [HelplineListComponent, HelplineFormComponent]
 })
 export class HelplineComponentsModule {}
