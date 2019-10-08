0d15d92997cff4d95fb38ebb6789fdf17088fa33
mansuriniyamat <mansuriniyamat.tech@gmail.com>
2019-10-08 11:52:33 +0530

    Dialog with actions

--- src/app/shared/dialog-modal/dialog-modal.component.ts
+++ src/app/shared/dialog-modal/dialog-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-modal',
@@ -9,9 +10,34 @@ export class DialogModalComponent implements OnInit {
 
   @Input() url: string;
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit() {
+
+  }
+
+  copy() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.data.value;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+  openURL() {
+    if (this.data.textType !== 'phone') {
+      window.open(this.data.value, '_blank');
+    }
+  }
+  close() {
+    this.dialogRef.close();
   }
 
 }
