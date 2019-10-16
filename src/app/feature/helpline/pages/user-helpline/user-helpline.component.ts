726233b218e5a09d3c4152d733b4c5381f60c4a1
Girish Dhiman <development.geektech@gmail.com>
2019-10-16 16:44:17 +0530

    added output emitter

--- src/app/feature/helpline/pages/user-helpline/user-helpline.component.ts
+++ src/app/feature/helpline/pages/user-helpline/user-helpline.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../../core/animations/route.animations';
-import { HelplineService } from '../../services/helpline.service';
-import { ConstantsService } from 'src/app/core/core.module';
-import { IState } from 'src/app/core/models/state';
+import { Component, OnInit } from "@angular/core";
+import { ROUTE_ANIMATIONS_ELEMENTS } from "../../../../core/animations/route.animations";
+import { HelplineService } from "../../services/helpline.service";
+import { ConstantsService } from "src/app/core/core.module";
+import { IState } from "src/app/core/models/state";
 
 @Component({
-  selector: 'app-user-helpline',
-  templateUrl: './user-helpline.component.html',
-  styleUrls: ['./user-helpline.component.scss']
+  selector: "app-user-helpline",
+  templateUrl: "./user-helpline.component.html",
+  styleUrls: ["./user-helpline.component.scss"]
 })
 export class UserHelplineComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   states: IState[];
-  selectedState = 'National';
+  selectedState = "National";
 
   helplines$ = this.helplineService.helplines$;
 
-  constructor(private helplineService: HelplineService,
-              private constantsService: ConstantsService) { }
+  constructor(
+    private helplineService: HelplineService,
+    private constantsService: ConstantsService
+  ) {}
 
   ngOnInit() {
     this.helplineService.findHelplines();
@@ -26,5 +28,8 @@ export class UserHelplineComponent implements OnInit {
 
   onStateChange(selectedState: string) {
     console.log(selectedState);
+  }
+  onSelectedGroupChanged(value: object) {
+    console.log(value, "testing");
   }
 }
--- src/app/shared/user-search/user-search.component.ts
+++ src/app/shared/user-search/user-search.component.ts
@@ -1,36 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { IUser } from "../../core/models/user"
-import { Subject, concat, of, Observable } from 'rxjs'
-import { UserService } from "../user-search/user.services"
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { IUser } from "../../core/models/user";
+import { Subject, concat, of, Observable } from "rxjs";
+import { UserService } from "../user-search/user.services";
 import {
   debounceTime,
   distinctUntilChanged,
   tap,
   switchMap,
   catchError
-} from 'rxjs/operators';
-
+} from "rxjs/operators";
 
 @Component({
-  selector: 'app-user-search',
-  templateUrl: './user-search.component.html',
-  styleUrls: ['./user-search.component.scss']
+  selector: "app-user-search",
+  templateUrl: "./user-search.component.html",
+  styleUrls: ["./user-search.component.scss"]
 })
 export class UserSearchComponent implements OnInit {
+  @Output() selectedgroup = new EventEmitter<IUser>();
   userList$: Observable<any>;
   userLoading = false;
   userInput$ = new Subject<string>();
-  selectedUser : IUser;
-  constructor(private userService: UserService) { }
+  selectedUser: IUser;
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.setupUserSearch()
+    this.setupUserSearch();
   }
-  trackByFn(item: IUser){
+  trackByFn(item: IUser) {
     return item._id;
   }
-
-  private setupUserSearch(){
+  onSelect() {
+    this.selectedgroup.emit(this.selectedUser);
+  }
+  private setupUserSearch() {
     this.userList$ = concat(
       of([]),
       this.userInput$.pipe(
@@ -44,6 +46,6 @@ export class UserSearchComponent implements OnInit {
           )
         )
       )
-    )
+    );
   }
 }
