94ccd2c2d73cbd266d323a858a268101587f2d9b
Girish Dhiman <development.geektech@gmail.com>
2019-10-16 16:48:52 +0530

    removed extra code

--- src/app/shared/user-search/user.services.ts
+++ src/app/shared/user-search/user.services.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { IUser } from '../../core/models/user';
-import { tap, distinctUntilChanged, map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { IUser } from "../../core/models/user";
+import { tap, distinctUntilChanged, map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
 
 const USER_URL = `api/users`;
 export interface UserState {
@@ -17,7 +17,7 @@ let _state: UserState = {
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
   private store = new BehaviorSubject<UserState>(_state);
@@ -43,58 +43,12 @@ export class UserService {
     return this.http.get(`${USER_URL}?username_contains=${term}`);
   }
 
-//   public findHelplineById(helplineId) {
-//     this.http
-//       .get(`${HELPLINE_URL}/${helplineId}`)
-//       .pipe(
-//         tap((inViewHelpline: IHelpline) => {
-//           this.updateState({
-//             ..._state,
-//             inViewHelpline
-//           });
-//         })
-//       )
-//       .subscribe();
-//   }
-
-//   searchHelpline(term: string) {
-//     return this.http.get(`${HELPLINE_URL}?name_contains=${term}`);
-//   }
-
-//   deleteHelpline(helplineId) {
-//     this.http.delete(`${HELPLINE_URL}/${helplineId}`).subscribe((res: any) => {
-//       const helplines = _state.helplines
-//         .filter(helpline => helpline._id !== helplineId)
-//         .slice();
-//       this.updateState({
-//         ..._state,
-//         helplines
-//       });
-//     });
-//   }
-
-//   addHelpline(helplineData: IHelpline) {
-//     this.http.post(`api/helplines`, helplineData).subscribe((res: any) => {
-//       const helplines = [res, ..._state.helplines];
-//       this.updateState({
-//         ..._state,
-//         helplines
-//       });
-//     });
-//   }
-
   public clearInViewUser() {
     this.updateState({
       ..._state,
       inViewUsers: null
     });
   }
-
-//   public updateHelpline(id: string, helpline) {
-//     return this.http.put(`${HELPLINE_URL}/${id}`, {
-//       ...helpline
-//     });
-//   }
 
   private updateState(state: UserState) {
     this.store.next((_state = state));
