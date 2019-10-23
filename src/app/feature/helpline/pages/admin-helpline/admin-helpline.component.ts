ae6ed65d4ef8a1cc0daeef471171c957991aa5a0
umarfarooq95 <mohamedumar931@gmail.com>
2019-10-23 07:32:31 +0530

    state selection render dynamic based on view for helpline screens

--- src/app/feature/helpline/pages/admin-helpline/admin-helpline.component.ts
+++ src/app/feature/helpline/pages/admin-helpline/admin-helpline.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HelplineService } from '../../services/helpline.service';
 import { IState } from 'src/app/core/models/state';
+import { LayoutService } from 'src/app/core/services/layout.service';
 @Component({
   selector: 'app-admin-helpline',
   templateUrl: './admin-helpline.component.html',
@@ -10,8 +11,12 @@ export class AdminHelplineComponent implements OnInit {
   states: IState[];
   selectedState = 'National';
   helplines$ = this.helplineService.helplines$;
+  isMobile$ = this.layoutService.isMobile$;
 
-  constructor(private helplineService: HelplineService) { }
+  constructor(
+    private helplineService: HelplineService,
+    private layoutService: LayoutService
+    ) { }
 
   ngOnInit() {
     this.helplineService.findHelplines();
--- src/app/feature/helpline/pages/user-helpline/user-helpline.component.ts
+++ src/app/feature/helpline/pages/user-helpline/user-helpline.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { ROUTE_ANIMATIONS_ELEMENTS } from "../../../../core/animations/route.animations";
-import { HelplineService } from "../../services/helpline.service";
-import { ConstantsService } from "src/app/core/core.module";
-import { IState } from "src/app/core/models/state";
+import { Component, OnInit } from '@angular/core';
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../../core/animations/route.animations';
+import { HelplineService } from '../../services/helpline.service';
+import { ConstantsService } from 'src/app/core/core.module';
+import { IState } from 'src/app/core/models/state';
+import { LayoutService } from 'src/app/core/services/layout.service';
 
 @Component({
-  selector: "app-user-helpline",
-  templateUrl: "./user-helpline.component.html",
-  styleUrls: ["./user-helpline.component.scss"]
+  selector: 'app-user-helpline',
+  templateUrl: './user-helpline.component.html',
+  styleUrls: ['./user-helpline.component.scss']
 })
 export class UserHelplineComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   states: IState[];
-  selectedState = "National";
+  selectedState = 'National';
 
   helplines$ = this.helplineService.helplines$;
+  isMobile$ = this.layoutService.isMobile$;
 
   constructor(
     private helplineService: HelplineService,
-    private constantsService: ConstantsService
+    private constantsService: ConstantsService,
+    private layoutService: LayoutService
   ) {}
 
   ngOnInit() {
@@ -30,6 +33,6 @@ export class UserHelplineComponent implements OnInit {
     console.log(selectedState);
   }
   onSelectedGroupChanged(value: object) {
-    console.log(value, "testing");
+    console.log(value, 'testing');
   }
 }
