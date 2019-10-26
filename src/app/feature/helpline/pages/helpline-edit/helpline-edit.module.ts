7f783b1805f96a8d566506151baee7cd5261bb14
umarfarooq95 <mohamedumar931@gmail.com>
2019-10-26 08:55:51 +0530

    button styles change

--- src/app/feature/helpline/pages/helpline-edit/helpline-edit.module.ts
+++ src/app/feature/helpline/pages/helpline-edit/helpline-edit.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HelplineEditRoutingModule } from './helpline-edit-routing.module';
 import { HelplineEditComponent } from './helpline-edit.component';
 import { HelplineComponentsModule } from '../../components/helpline.components.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [HelplineEditComponent],
   imports: [
     CommonModule,
+    SharedModule,
     HelplineEditRoutingModule,
     HelplineComponentsModule
   ]
