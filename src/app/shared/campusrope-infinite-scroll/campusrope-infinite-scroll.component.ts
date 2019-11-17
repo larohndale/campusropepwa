d0e915d9d13dd3a4804192e93a75d25ee97da6d3
sujit <sujit.patil72@hotmail.com>
2019-11-17 16:00:35 +0530

    added infinite scrolling

--- src/app/shared/campusrope-infinite-scroll/campusrope-infinite-scroll.component.ts
+++ src/app/shared/campusrope-infinite-scroll/campusrope-infinite-scroll.component.ts
@@ -1,37 +1,95 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-
+import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { merge } from 'rxjs';
+import { distinct, filter, map, debounceTime, tap, flatMap } from 'rxjs/operators';
+import * as _ from 'lodash';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'campusrope-infinite-scroll',
   templateUrl: './campusrope-infinite-scroll.component.html',
   styleUrls: ['./campusrope-infinite-scroll.component.scss']
 })
-export class CampusRopeIScrollComponent implements OnInit {
-
-  @Input() apiurl : string;
-  @Input() datasource  : any[];
-  @Input() youtube  : boolean;
+export class CampusRopeIScrollComponent {
+  private _jsonURL = '.../../../assets/youtube.json';
   
-  public sampledatasource : any[];
-
-  constructor(
-    
-  ) { }
-
-  ngOnInit() {
-
-    this.sampledatasource = [];
-    this.sampledatasource.push( { "title" : "mytitle" });
-    this.sampledatasource.push( { "title" : "safsdf" });
-    this.sampledatasource.push( { "title" : "gssadgsda" });
-    this.sampledatasource.push( { "title" : "asfdsfad" });
-    this.sampledatasource.push( { "title" : "rewrwrqer" });
-    this.sampledatasource.push( { "title" : "sffsdafsd" });
-    this.sampledatasource.push( { "title" : "sfdfsadfsad" });
-    this.sampledatasource.push( { "title" : "sadffsdsfa" });
-
+  public getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
   }
+
+getYoutubeData() {
+  return new Promise((resolve) => {
+    this.getJSON()
+      .subscribe(data => {
+      console.log("TCL: CampusRopeIScrollComponent -> getYoutubeData -> data", data)
+        this._ytData = data;
+        resolve()
+      });
+  })
+}
+
+_ytData: [];
+private cache = [];
+private pageByManual$ = new BehaviorSubject(1);
+private itemHeight = 400;
+private numberOfItems = 5;
+private pageByScroll$ = fromEvent(window, "scroll")
+  .pipe(
+    map(() => window.scrollY),
+    filter(current => current >= document.body.clientHeight - window.innerHeight),
+    debounceTime(200),
+    distinct(),
+    map(y => Math.ceil((y + window.innerHeight) / (this.itemHeight * this.numberOfItems)))
+  );
+
+private pageByResize$ = fromEvent(window, "resize")
+  .pipe(
+    debounceTime(200),
+    map(_ => Math.ceil(
+      (window.innerHeight + document.body.scrollTop) /
+      (this.itemHeight * this.numberOfItems)
+    ))
+  )
+
+pageToLoad$: Observable<any>;
+itemResults$: Observable<any>;
+constructor(private http: HttpClient) {
+  this.init()
+}
+
+async init() {
+  await this.getYoutubeData()
+  this.pageToLoad$ = merge(this.pageByManual$, this.pageByScroll$, this.pageByResize$)
+    .pipe(
+      distinct(),
+      filter(page => this.cache[page - 1] === undefined)
+    );
+
+  this.itemResults$ = this.pageToLoad$
+    .pipe(
+      flatMap((page: number) => {
+        return this.getYotubeData(page)
+          .pipe(
+            tap(resp => {
+              this.cache[page - 1] = resp;
+              console.log("TCL: AppComponent -> this.cache", this.cache)
+              if ((this.itemHeight * this.numberOfItems * page) < window.innerHeight) {
+                this.pageByManual$.next(page + 1);
+              }
+            })
+          )
+      }),
+      map(() => _.flatMap(this.cache))
+    );
+}
+
+getYotubeData(page): Observable<any> {
+  return new Observable((observer) => {
+    observer.next([... this._ytData.slice(5 * (page - 1), (this.numberOfItems * (page - 1)) + this.numberOfItems)])
+    observer.complete()
+  })
+}
 
   
 
--- src/app/shared/utils/safe.pipe.ts
+++ src/app/shared/utils/safe.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
 
 @Pipe({
   name: 'safe'
 })
 export class SafePipe implements PipeTransform {
 
+  constructor(protected sanitizer: DomSanitizer) {}
+
   transform(value: any, ...args: any[]): any {
-    return null;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(value);
   }
 
 }
