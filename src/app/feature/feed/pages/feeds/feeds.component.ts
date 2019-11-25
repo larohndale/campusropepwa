18d573bf55c4cd38e61083c51856d29a871626e1
sanjay <sanjaykumar@i-serve.com.my>
2019-11-25 12:34:22 +0800

    implemented the youtube feed task

--- src/app/feature/feed/pages/feeds/feeds.component.ts
+++ src/app/feature/feed/pages/feeds/feeds.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Pipe, PipeTransform , ViewChild, AfterViewChecked, ElementRef} from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 @Pipe({ name: 'safe' })
+
 export class SafePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
   transform(url) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
+
 @Component({
   selector: 'app-feeds',
   templateUrl: './feeds.component.html',
@@ -14,7 +16,6 @@ export class SafePipe implements PipeTransform {
 })
 
 export class FeedsComponent implements OnInit  {
-  
   
   _ytData = [{
     "name": 1,
@@ -50,64 +51,64 @@ export class FeedsComponent implements OnInit  {
 
   }]
   
-  _ytUpdatedData:any[];
-  defaultYTV:any = this._ytData[0];
+ 
   active:boolean = true
   oldScrollTop = 0
   count = 0
   scrollTop: number = 0;
-  offsetHeight: any;
-  currentScrollTop: any;
-  constructor(public sanitizer: DomSanitizer) { 
-    this._ytUpdatedData = this._ytData.filter((e,i)=> i!=0);
-    console.log(this._ytUpdatedData)
-  }
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this._ytData.map((iframe,i)=> i==0?iframe.iframe = iframe.iframe+'?autoplay=1&mute=1':iframe.iframe)
     window.addEventListener('scroll', this.scroll, true); 
   }
  
   ngOnDestroy() {
       window.removeEventListener('scroll', this.scroll, true);
   }
-  // trackById(index, contact) {
-  //     // console.log(contact)
-  //   return contact.id;
-  // }
   scroll = (e): void => {  
     if(this.active){
       setTimeout(()=>{
-       this.callback(e)
-      },500)
-   }
-   this.active = false
-  };
-  callback(e){
-    console.log(e)
-    // this.scrollTop =e.srcElement.scrollTop 
-    this.offsetHeight =e.srcElement.offsetHeight 
-    this.currentScrollTop = e.srcElement.scrollTop
-
-    if (e.srcElement.scrollTop > this.scrollTop ){
-      console.log('increased')
-      this.scrollTop += e.srcElement.offsetHeight
-              this.count++
-   
-    } 
-    if(this.oldScrollTop > e.srcElement.scrollTop){
-      console.log('decreased')
-      this.count -= 1
-      this.scrollTop -= e.srcElement.offsetHeight
-
+        this.callback(e)
+      },1000)
     }
-    var element = document.getElementById('iframe'+this.count);
-    // this.defaultYTV = this._ytData[this.count]
-   
-    // this._ytUpdatedData = this._ytData.filter((e,i)=> i!=this.count);
-    
-    console.log(this.count)
+    this.active = false
+  };
+
+  callback(e){
+    if (e.srcElement.scrollTop > this.scrollTop ) {
+
+        this.scrollTop += e.srcElement.offsetHeight/2
+        this.count++
+    } 
+
+    if (this.oldScrollTop > e.srcElement.scrollTop) {
+        if (this.count == 0) {
+            this.count = 0
+        } else {
+            this.count -= 1
+        }
+        this.scrollTop -= e.srcElement.offsetHeight/2
+    }
+
+    this._ytData.map((iframe,i)=> i===this.count?iframe.iframe = iframe.iframe+'?autoplay=1&mute=1':iframe.iframe =this.removeURLParameter(iframe.iframe))
     this.active =  true
+
     this.oldScrollTop = e.srcElement.scrollTop
+  }
+
+  removeURLParameter(url) {
+      var oldURL = url
+      var index = 0;
+      var newURL = oldURL;
+      index = oldURL.indexOf('?');
+      if(index == -1){
+          index = oldURL.indexOf('#');
+      }
+      if(index != -1){
+          newURL = oldURL.substring(0, index);
+      }
+      return newURL;
   }
 
 }
