c1fb6c3b056c41a6d16064f3eda01e740f0454f2
faiyazbits <m.faiyaz123@gmail.com>
2019-12-01 09:11:06 +0530

    fixed helpline list style encapsulation issue

--- src/app/feature/helpline/components/helpline-list/helpline-list.component.ts
+++ src/app/feature/helpline/components/helpline-list/helpline-list.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
-import { IHelpline } from 'src/app/core/models/helpline';
-import { Observable } from 'rxjs';
-import {DialogModalComponent} from '../../../../shared/dialog-modal/dialog-modal.component';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import { IHelpline } from "src/app/core/models/helpline";
+import { Observable } from "rxjs";
+import { DialogModalComponent } from "../../../../shared/dialog-modal/dialog-modal.component";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-helpline-list',
-  templateUrl: './helpline-list.component.html',
-  styleUrls: ['./helpline-list.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-helpline-list",
+  templateUrl: "./helpline-list.component.html",
+  styleUrls: ["./helpline-list.component.scss"]
 })
 export class HelplineListComponent implements OnInit {
   @Input() helplineList$: Observable<IHelpline[]>;
@@ -18,13 +21,13 @@ export class HelplineListComponent implements OnInit {
 
   goToLink(type: string, url: string) {
     const dialogRef = this.dialog.open(DialogModalComponent, {
-      width: '200px',
-      data: {textType: type, value: url}
+      width: "200px",
+      data: { textType: type, value: url }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
+      console.log("The dialog was closed");
     });
-   // window.open(url, '_blank');
+    // window.open(url, '_blank');
   }
 }
