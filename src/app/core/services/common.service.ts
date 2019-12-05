e156d1fffb4acc9e17d859286271a4493af7a028
sujit <sujit.patil72@hotmail.com>
2019-12-05 22:05:47 +0530

    changed name from _trendingAPI to commonApi

--- src/app/core/services/common.service.ts
+++ src/app/core/services/common.service.ts
@@ -10,22 +10,22 @@ export class CommonService {
 
   contentPerPage: number = 100
   constructor(private http: HttpClient) { }
-  private _trendingAPI: string = 'https://vast-sands-97057.herokuapp.com/trendings';
+  private _commonAPI: string = 'https://vast-sands-97057.herokuapp.com/trendings';
   getTrendingData(page = 1): Observable<ITrending[]> {
-    const urlWithPagination = `${this._trendingAPI}?_start=${(this.contentPerPage * page) - (this.contentPerPage - 1)}&_limit=${this.contentPerPage}`
+    const urlWithPagination = `${this._commonAPI}?_start=${(this.contentPerPage * page) - (this.contentPerPage - 1)}&_limit=${this.contentPerPage}`
     return this.http.get<ITrending[]>(urlWithPagination);
   }
   getTrendingDataBy(parameter, value): Observable<ITrending[]> {
-    const url = `${this._trendingAPI}?${parameter}=${value}`
+    const url = `${this._commonAPI}?${parameter}=${value}`
     return this.http.get<ITrending[]>(url);
   }
   addTrendingData(data: ITrending): Observable<any> {
-    return this.http.post(this._trendingAPI, data);
+    return this.http.post(this._commonAPI, data);
   }
   updateTrendingData(data: ITrending, id): Observable<any> {
-    return this.http.put(`${this._trendingAPI}/${id}`, data);
+    return this.http.put(`${this._commonAPI}/${id}`, data);
   }
   deleteTrendingData(id): Observable<any> {
-    return this.http.delete(`${this._trendingAPI}/${id}`);
+    return this.http.delete(`${this._commonAPI}/${id}`);
   }
 }
