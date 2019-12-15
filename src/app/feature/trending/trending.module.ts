31f1777ae541e5c259e9c4e09b18cfed7dfdf051
sujit <sujit.patil72@hotmail.com>
2019-12-15 20:27:45 +0530

    ceaated client component

--- src/app/feature/trending/trending.module.ts
+++ src/app/feature/trending/trending.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 
 import { TrendingRoutingModule } from "./trending-routing.module";
 import { TrendingListComponent } from "./components/trending-list/trending-list.component";
+import { ClientsComponent } from './pages/clients/clients.component';
 
 @NgModule({
-  declarations: [TrendingListComponent],
+  declarations: [TrendingListComponent, ClientsComponent],
   imports: [TrendingRoutingModule, CommonModule]
 })
 export class TrendingModule {}
