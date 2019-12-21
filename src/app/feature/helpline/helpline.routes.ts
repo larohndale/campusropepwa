3ad7cf6528781561ed3201bbe59287d98f93661b
faiyazbits <m.faiyaz123@gmail.com>
2019-12-21 09:57:22 +0530

    added auth-gaurds for admintask and helpline routes

--- src/app/feature/helpline/helpline.routes.ts
+++ src/app/feature/helpline/helpline.routes.ts
@@ -1,33 +1,37 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AuthGuardService } from "src/app/core/core.module";
 
 export const helplineRoutes: Routes = [
   {
-    path: '',
+    path: "",
     loadChildren: () =>
-      import('./pages/user-helpline/user-helpline.module').then(
+      import("./pages/user-helpline/user-helpline.module").then(
         m => m.UserHelplineModule
       )
   },
   {
-    path: 'admin',
+    path: "admin",
     loadChildren: () =>
-      import('./pages/admin-helpline/admin-helpline.module').then(
+      import("./pages/admin-helpline/admin-helpline.module").then(
         m => m.AdminHelplineModule
-      )
+      ),
+    canActivate: [AuthGuardService]
   },
   {
-    path: 'add',
+    path: "add",
     loadChildren: () =>
-      import('./pages/helpline-add/helpline-add.module').then(
+      import("./pages/helpline-add/helpline-add.module").then(
         m => m.HelplineAddModule
-      )
+      ),
+    canActivate: [AuthGuardService]
   },
   {
-    path: 'edit/:id',
+    path: "edit/:id",
     loadChildren: () =>
-      import('./pages/helpline-edit/helpline-edit.module').then(
+      import("./pages/helpline-edit/helpline-edit.module").then(
         m => m.HelplineEditModule
-      )
+      ),
+    canActivate: [AuthGuardService]
   }
 ];
 
