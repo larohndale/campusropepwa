51d48fe6ac88fcc934adb123df89e4335bbf6178
faiyazbits <m.faiyaz123@gmail.com>
2019-12-21 09:57:22 +0530

    added auth-gaurds for admintask and helpline routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+import { AuthGuardService } from "./core/core.module";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./shell/shell.module').then(m => m.ShellModule)
+    path: "",
+    loadChildren: () => import("./shell/shell.module").then(m => m.ShellModule)
   },
   {
-    path: 'login',
+    path: "login",
     loadChildren: () =>
-      import('./feature/login/login.module').then(m => m.LoginModule)
+      import("./feature/login/login.module").then(m => m.LoginModule)
   },
   {
-    path: 'admin-task',
+    path: "admin-task",
     loadChildren: () =>
-      import('./feature/admin-task/admin-task.module').then(
+      import("./feature/admin-task/admin-task.module").then(
         m => m.AdminTaskModule
-      )
+      ),
+    canActivate: [AuthGuardService]
   },
   {
-    path: '**',
-    redirectTo: ''
+    path: "**",
+    redirectTo: "/"
   }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
+      scrollPositionRestoration: "enabled",
       preloadingStrategy: PreloadAllModules
     })
   ],
