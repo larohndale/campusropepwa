64c7099595fa30a320100c4da04e104a70c7eb43
sujit <sujit.patil72@hotmail.com>
2020-01-01 21:35:56 +0530

    changed result.name to tiltle

--- src/app/feature/trending/pages/admin-trending/admin-trending.component.ts
+++ src/app/feature/trending/pages/admin-trending/admin-trending.component.ts
@@ -1,52 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonService } from 'src/app/core/services/common.service';
-import { TableNames } from '../../../../core/config/TableNames';
-import { MatDialog, } from '@angular/material/dialog';
-import { AdminTrendingStatesDialoug } from './admin-trending-states-dialoug/admin-trending-states-dialoug.component';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { CommonService } from "src/app/core/services/common.service";
+import { TableNames } from "../../../../core/config/TableNames";
+import { MatDialog } from "@angular/material/dialog";
+import { AdminTrendingStatesDialoug } from "./admin-trending-states-dialoug/admin-trending-states-dialoug.component";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-admin-trending',
-  templateUrl: './admin-trending.component.html',
-  styleUrls: ['./admin-trending.component.scss']
+  selector: "app-admin-trending",
+  templateUrl: "./admin-trending.component.html",
+  styleUrls: ["./admin-trending.component.scss"]
 })
 export class AdminTrendingComponent implements OnInit {
   selectedState: string = "Select State";
 
-  constructor(public dialog: MatDialog, public readonly commonService: CommonService) { }
+  constructor(
+    public dialog: MatDialog,
+    public readonly commonService: CommonService
+  ) {}
 
   dialogueConfig(data) {
-    const elem = document.querySelector(".mat-toolbar-single-row")
+    const elem = document.querySelector(".mat-toolbar-single-row");
     const config = {
       panelClass: "admin-trending-dialougs",
-      width: window.innerWidth + 'px',
-      height: window.innerHeight - elem.clientHeight + 'px',
+      width: window.innerWidth + "px",
+      height: window.innerHeight - elem.clientHeight + "px",
       data: data,
       disableClose: true
-    }
-    return (component) => this.dialog.open(component, config);
+    };
+    return component => this.dialog.open(component, config);
   }
 
   openStatesDialog(): void {
-    this.commonService.getState()
-      .subscribe(states => {
-        const dialogRef = this.dialogueConfig(states)(AdminTrendingStatesDialoug)
-        dialogRef.afterClosed().
-          subscribe(result => {
-            this.selectedState = result.name
-          });
-      })
+    this.commonService.getState().subscribe(states => {
+      const dialogRef = this.dialogueConfig(states)(AdminTrendingStatesDialoug);
+      dialogRef.afterClosed().subscribe(result => {
+        this.selectedState = result.title;
+      });
+    });
   }
 
   ngOnInit() {
-
     /* this.commonService.getData()
       .subscribe(data => {
         // console.log("TCL: AdminTrendingComponent -> ngOnInit -> data", data)
       }) */
   }
-
-
 }
-
-
