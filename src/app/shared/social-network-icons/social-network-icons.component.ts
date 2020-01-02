3d84034bc316d47caa2d8273ea251de912cbfc98
faiyazbits <m.faiyaz123@gmail.com>
2020-01-02 21:12:01 +0530

    social icons styles adjusted and changed images

--- src/app/shared/social-network-icons/social-network-icons.component.ts
+++ src/app/shared/social-network-icons/social-network-icons.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IHelpline } from 'src/app/core/models/helpline';
+import { Component, OnInit, Input } from "@angular/core";
+import { Observable } from "rxjs";
+import { IHelpline } from "src/app/core/models/helpline";
 
 @Component({
-  selector: 'app-social-network-icons',
-  templateUrl: './social-network-icons.component.html',
-  styleUrls: ['./social-network-icons.component.scss']
+  selector: "app-social-network-icons",
+  templateUrl: "./social-network-icons.component.html",
+  styleUrls: ["./social-network-icons.component.scss"]
 })
 export class SocialNetworkIconsComponent implements OnInit {
-
   @Input() helpline: IHelpline;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getStyles(url) {
+    return {
+      "pointer-events": url ? "auto" : "none",
+      opacity: url ? "1" : "0.2"
+    };
   }
-
 }
